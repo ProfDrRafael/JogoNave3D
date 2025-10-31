4ff7f7c7bdc08ad985f109c80e1d8d25289a030c
Rafael Canteri <pacifista_s@hotmail.com>
2025-10-31 16:58:16 -0400

    Primeiro commit

---
+++ SpaceGame3D/Assets/Scripts/Jogador.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int forcaX, forcaZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb.AddForce(0, 0, forcaZ);
+        //Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forcaZ);
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-forcaX * Time.fixedDeltaTime, 0, 0);
+        }
+        else if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(forcaX * Time.fixedDeltaTime, 0, 0);
+        }
+        //Debug.Log("Update");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Jogador Colidiu com " + collision.collider.name);
+    }
+}
---
+++ SpaceGame3D/Assets/Scripts/SeguirJogador.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirJogador : MonoBehaviour
+{
+    public Jogador jogador;
+    public Vector3 distanciaJogador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = jogador.transform.position + distanciaJogador;
+    }
+}
